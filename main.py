7052a132925a4b50bb3e37d91b703a83b0563b93
Sandeep Kumar <58764439+sandkum5@users.noreply.github.com>
2021-12-16 14:58:34 -0800

    Update main.py

--- main.py
+++ main.py
@@ -17,60 +17,34 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def create_workspace(org_name, tfcloud_api_token, payload):
+def api_request(URL, METHOD, PAYLOAD=None):
+    TFCLOUD_API_TOKEN = os.getenv("tfcloud_api_token")
+    HEADERS = {
+        "Content-Type": "application/vnd.api+json",
+        "Authorization": "Bearer " + TFCLOUD_API_TOKEN,
+    }
+    response = requests.request(
+        method=METHOD, url=URL, headers=HEADERS, data=json.dumps(PAYLOAD)
+    )
+    return response
+
+
+def workspace_create(org_name, payload):
     """
     Function to create workspace in tfcloud in the provided organization with Intersight hyperlink
     """
-    headers = {
-        "Content-Type": "application/vnd.api+json",
-        "Authorization": "Bearer " + tfcloud_api_token,
-    }
     url = f"https://app.terraform.io/api/v2/organizations/{org_name}/workspaces"
-    response = requests.post(
-        url,
-        headers=headers,
-        data=json.dumps(payload),
-    )
-    # print(response.json())
+    response = api_request(URL=url, METHOD="POST", PAYLOAD=payload)
     print(f"Workspace Create Status: {response.status_code}")
 
 
-def update_workspace(workspace_id, payload):
-    """
-    ** Doesn't Work
-    Function to create a workspace under the provided organization
-    """
-    # workspace_name = ""
-    # organization_name = ""
-    # url = f"https://app.terraform.io/api/v2/organizations/{organization_name}/workspaces/{workspace_name}"
-    headers = {
-        "Content-Type": "application/vnd.api+json",
-        "Authorization": "Bearer O2hKX3r8zakeOQ.atlasv1.QbAlExUIoSAoVMdmmzIxW5Js5LS2LFnQbnG1FydnJgBBWBcNAW5OK81way89ZmSm1xU",
-    }
-    url = f"https://app.terraform.io/api/v2/workspaces/{workspace_id}"
-    response = requests.patch(
-        url,
-        headers=headers,
-        data=json.dumps(payload),
-    )
-    print(response.json())
-    print(f"Workspace Create Status: {response.status_code}")
-
-
-def get_workspace(tfcloud_api_token, org_name):
+def workspace_get(org_name):
     """
     Function to list workspaces under an Organization
     """
     workspace_data = {}
-    headers = {
-        "Content-Type": "application/vnd.api+json",
-        "Authorization": "Bearer " + tfcloud_api_token,
-    }
     url = f"https://app.terraform.io/api/v2/organizations/{org_name}/workspaces"
-    response = requests.get(
-        url,
-        headers=headers,
-    )
+    response = api_request(URL=url, METHOD="GET")
     response_data = response.json()
     for workspace in response_data["data"]:
         workspace_id = workspace["id"]
@@ -78,53 +52,43 @@ def get_workspace(tfcloud_api_token, org_name):
     return workspace_data
 
 
-def create_workspace_var(tfcloud_api_token, workspace_id, payload):
+def workspace_variable_create(workspace_id, payload):
     """
     Function to create variables under workspaces
     """
-    headers = {
-        "Content-Type": "application/vnd.api+json",
-        "Authorization": "Bearer " + tfcloud_api_token,
-    }
     url = f"https://app.terraform.io/api/v2/workspaces/{workspace_id}/vars"
-    response = requests.post(
-        url,
-        headers=headers,
-        data=json.dumps(payload),
-    )
+    response = api_request(URL=url, METHOD="POST", PAYLOAD=payload)
     print(f"Workspace Variable Create Status: {response.status_code}")
 
 
-def create_workspace_vars(tfcloud_api_token, workspace_id, var_list):
+def workspace_variables_create_multi(workspace_id, variable_list):
     """
     Function to create multiple variables in a workspace
     """
-    for var in var_list:
+    for variable in variable_list:
         payload = {
             "data": {
                 "type": "vars",
                 "attributes": {
-                    "key": var["key"],
-                    "value": var["value"],
-                    "description": var["description"],
-                    "category": var["category"],
+                    "key": variable["key"],
+                    "value": variable["value"],
+                    "description": variable["description"],
+                    "category": variable["category"],
                     "hcl": False,
                     "sensitive": False,
                 },
             }
         }
-        var_name = payload["data"]["attributes"]["key"]
-        print(f"Creating WorkSpace Var: {var_name}")
-        create_workspace_var(tfcloud_api_token, workspace_id, payload)
+        variable_name = payload["data"]["attributes"]["key"]
+        print(f"Creating WorkSpace Var: {variable_name}")
+        workspace_variable_create(workspace_id, payload)
 
 
 def main():
     """
     Main Function to execute the code
     """
-    tfcloud_api_token = os.getenv("tfcloud_api_token")
-
-    variable_file = "api_data.json"
+    variable_file = "tfcloud_vars.json"
     with open(variable_file, "r") as file:
         data = json.load(file)
 
@@ -147,10 +111,10 @@ def main():
     # Create Workspace
     workspace_name = workspace_payload["data"]["attributes"]["name"]
     print(f"Creating workspace: {workspace_name}")
-    create_workspace(org_name, tfcloud_api_token, workspace_payload)
+    workspace_create(org_name, workspace_payload)
 
     # List workspaces
-    workspace_data = get_workspace(tfcloud_api_token, org_name)
+    workspace_data = workspace_get(org_name)
 
     # Set workspace ID for which we are creating variables
     for key, value in workspace_data.items():
@@ -158,10 +122,10 @@ def main():
             workspace_id = key
 
     # Set Variables Payload
-    var_list = data["vars"]
+    variable_list = data["vars"]
 
-    # Create variables defined in api_data.json file
-    create_workspace_vars(tfcloud_api_token, workspace_id, var_list)
+    # Create variables defined in tfcloud_vars.json file
+    workspace_variables_create_multi(workspace_id, variable_list)
 
 
 if __name__ == "__main__":
